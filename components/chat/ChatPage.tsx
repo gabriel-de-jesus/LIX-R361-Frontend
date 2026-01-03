1882e8819c41fc9e3205be772016801fd78fadfa
gabriel-de-jesus <mestregabrieldejesus@gmail.com>
2026-01-03 19:37:13 +0900

    Update chat page

--- components/chat/ChatPage.tsx
+++ components/chat/ChatPage.tsx
@@ -382,7 +382,7 @@ export default function ChatPage() {
                 });
                 // Small delay to mimic natural typing
                 // eslint-disable-next-line no-await-in-loop
-                await new Promise(resolve => setTimeout(resolve, 10));
+                await new Promise(resolve => setTimeout(resolve, 60));
               }
             }
           }
